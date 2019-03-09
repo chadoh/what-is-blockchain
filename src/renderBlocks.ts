062c8b6d15f7eb5cf1c7ed6998f302f34f5f1ffe
Chad Ostrowski <hi@chadoh.com>
2019-03-09 09:49:50 -0500

    Make blocks 3D

--- src/renderBlocks.ts
+++ src/renderBlocks.ts
@@ -5,12 +5,27 @@ export default blocks => `<ol>${blocks.map(block => {
   const dateMined = (new Date(parseInt(block.timestamp) * 1000)).toLocaleString()
 
   return `
-    <li>
+    <li class="block">
       <header>
         <span class="blockNumber">${blockNumber}</span>
         <span class="dateMined">${dateMined}</span>
       </header>
-      <button class="transactionCount">${block.transactions.length} transactions</button>
+      <h2 class="blockNumber">${blockNumber}</h2>
+      <div class="dateMined">${dateMined}</div>
+      <div>${block.transactions.length} transactions</div>
+      <div class="transactions">
+        ${block.transactions.map((transaction, i) => {
+          const amount = parseInt(transaction.value, 16)
+
+          return `
+            <div class="transaction" style="transform: translateZ(calc(.2em * ${i}))">
+              <div class="from">${transaction.from}</div> sent
+              <div class="amount">${amount}</div> to
+              <div class="to">${transaction.to}</div>
+            </div>
+          `
+        }).join("")}
+      </div>
     </li>
   `
 }).join("")}</ol>`
