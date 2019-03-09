8962f52bf736bbc359eaf4d8fdd1003182c589ba
Chad Ostrowski <hi@chadoh.com>
2019-03-09 12:32:19 -0500

    Set up blocks for flipping

--- src/renderBlocks/index.ts
+++ src/renderBlocks/index.ts
@@ -1,24 +1,27 @@
 import "./blocks.css"
+import renderShape from "./renderShape"
 
-export default blocks => `<ol>${blocks.map(block => {
+export default blocks => blocks.map(block => {
   const blockNumber = parseInt(block.number, 16)
   const dateMined = (new Date(parseInt(block.timestamp) * 1000)).toLocaleString()
 
-  return `
-    <li class="block">
+  return renderShape({
+    front: `
+      <h2 class="blockNumber">${blockNumber}</h2>
+      <div class="dateMined">${dateMined}</div>
+      <div>${block.transactions.length} transactions</div>
+    `,
+    back: `
       <header>
         <span class="blockNumber">${blockNumber}</span>
         <span class="dateMined">${dateMined}</span>
       </header>
-      <h2 class="blockNumber">${blockNumber}</h2>
-      <div class="dateMined">${dateMined}</div>
-      <div>${block.transactions.length} transactions</div>
       <div class="transactions">
         ${block.transactions.map((transaction, i) => {
           const amount = parseInt(transaction.value, 16)
 
           return `
-            <div class="transaction" style="transform: translateZ(calc(.2em * ${i}))">
+            <div class="transaction">
               <div class="from">${transaction.from}</div> sent
               <div class="amount">${amount}</div> to
               <div class="to">${transaction.to}</div>
@@ -26,6 +29,6 @@ export default blocks => `<ol>${blocks.map(block => {
           `
         }).join("")}
       </div>
-    </li>
-  `
-}).join("")}</ol>`
+    `
+  })
+}).join("")
---
+++ src/renderBlocks/renderShape.ts
@@ -0,0 +1,8 @@
+export default ({ front, back }) => `
+  <div class="block">
+    ${front}
+    <div class="block-back">
+      ${back}
+    </div>
+  </div>
+`
