bf3bdd5853a3aeea63faba760a1e14e92dbf5d3b
Chad Ostrowski <hi@chadoh.com>
2019-03-09 21:57:10 -0500

    Animate block rotating to show back
    The back is currently unstyled.
    
    Things that make me sad:
    
    * buttons don't work well with 3D transformed elements, so I'm using a
      link instead
    
    * position: fixed doesn't work the usual way with 3D scenes, hence the
      adding of `window.scrollY`
    
    * finding the x and y position of a 3D transformed element does not work
      as expected, which makes it hard to place a fixed element over a
      previous element. I was able to get it looking ok for blocks near the
      top, but it gets progressively off the more you scroll.
    
      It also looks really bad on wider viewports!
    
      And has serious cross-browser issues!

--- src/renderBlocks/index.ts
+++ src/renderBlocks/index.ts
@@ -1,5 +1,90 @@
 import "./blocks.css"
 import renderShape from "./renderShape"
+import cloneNode from "./cloneNode"
+
+const animationLength = 0.5
+
+document.addEventListener("click", e => {
+  const target = e.target as HTMLElement
+  if (target.dataset.behavior === "open-block") {
+    e.preventDefault()
+    openBlock(target.closest(".block") as HTMLElement)
+  }
+})
+
+const openBlock = (block: HTMLElement): void => {
+  // 1. duplicate the element, add to bottom of DOM, fix position above old element
+  const clone = block.cloneNode(true) as HTMLElement
+  const rect = block.getBoundingClientRect()
+  let styles = `
+    position: fixed;
+    top: calc(${rect.top + window.scrollY}px + 1em);
+    left: calc(${rect.left}px - 1.1em);
+    width: ${block.offsetWidth}px;
+    height: ${block.offsetHeight}px;
+    transform: rotateY(-30deg) scaleX(1.1) translateZ(1em);
+    transition: all ${animationLength}s;
+  `
+  clone.style.cssText = clone.style.cssText + ";" + styles
+
+  // save values for closing block later
+  clone.dataset.top = clone.style.top
+  clone.dataset.left = clone.style.left
+  clone.dataset.width = clone.style.width
+  clone.dataset.height = clone.style.height
+
+  document.body.appendChild(clone)
+
+  // 2. set visibility=hidden using class which can be removed when closing
+  block.className = block.className + " opened"
+
+  // 3. make full screen
+  styles = `
+    position: fixed;
+    top: calc(${window.scrollY}px + 1em);
+    left: 1em;
+    width: calc(100% - 2em);
+    height: calc(100vh - 3em);
+    transform: perspective(40em) rotateY(-180deg);
+  `
+  clone.style.cssText = clone.style.cssText + ";" + styles
+
+  // TODO: remove me!
+  setTimeout(() => closeBlock(clone), 2000)
+}
+
+const closeBlock = function(clone: HTMLElement): void {
+  // 1. set back to original dimensions
+  let styles = `
+    position: fixed;
+    top: ${clone.dataset.top};
+    left: ${clone.dataset.left};
+    width: ${clone.dataset.width};
+    height: ${clone.dataset.height};
+    transform: rotateY(-30deg) scaleX(1.1) translateZ(1em);
+  `
+  clone.style.cssText = clone.style.cssText + ";" + styles
+
+  // 2. wait for animation to complete
+  setTimeout(() => {
+    // 3. remove fixed styling
+    styles = `
+      position: static;
+      top: auto;
+      left: auto;
+      width: auto;
+      height: auto;
+    `
+    clone.style.cssText = clone.style.cssText + ";" + styles
+
+    // 4. make original block visible again
+    const block = document.querySelector(".block.opened")
+    block.className = block.className.replace(/ opened/, "")
+
+    // 5. remove placeholder
+    clone.remove()
+  }, animationLength * 1000)
+};
 
 export default blocks => blocks.map(block => {
   const blockNumber = parseInt(block.number, 16)
@@ -9,7 +94,18 @@ export default blocks => blocks.map(block => {
     front: `
       <h2 class="blockNumber">${blockNumber}</h2>
       <div class="dateMined">${dateMined}</div>
-      <div>${block.transactions.length} transactions</div>
+      <div>
+        ${block.transactions.length} transactions
+        ${!block.transactions.length ? "" : `
+          –
+          <a
+            href="#${block.number}"
+            data-behavior="open-block"
+          >
+            View
+          </a>
+        `}
+      </div>
     `,
     back: `
       <header>
