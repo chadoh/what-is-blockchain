332d91e66334fd6e5e029bf039b6082e878d54a4
Chad Ostrowski <hi@chadoh.com>
2019-03-16 12:19:19 -0400

    Improve accessibility and Safari Reader view

--- src/renderBlocks/index.ts
+++ src/renderBlocks/index.ts
@@ -33,43 +33,48 @@ export default blocks => blocks.map(block => {
 
   return `
     <div class="block">
-      <h2 class="blockNumber">${blockNumber}</h2>
-      <div class="dateMined">${dateMined}</div>
-      <div>
-        ${block.transactions.length} transactions
-        ${!block.transactions.length ? "" : `
-          –
-          <a
-            href="#${block.number}"
-            data-behavior="open-block"
-          >
-            View
-          </a>
-        `}
+      <div aria-hidden="true">
+        <h2 class="blockNumber">${blockNumber}</h2>
+        <div class="dateMined">${dateMined}</div>
+        <div>
+          ${block.transactions.length} transactions
+          ${!block.transactions.length ? "" : `
+            –
+            <a
+              href="#${block.number}"
+              data-behavior="open-block"
+            >
+              View
+            </a>
+          `}
+        </div>
       </div>
       <div class="block-back">
         <div class="inner" data-behavior="inner-height">
           <header>
-            <h2 class="blockNumber">${blockNumber}</h2>
+            <div>
+              <h2 class="blockNumber">${blockNumber}</h2>
+              <div class="dateMined">${dateMined}</div>
+            </div>
             <a href="#close-block">&times;</a>
           </header>
 
           <div>${block.transactions.length} transaction${block.transactions.length > 1 ? 's' : ''}</div>
-          <div class="transactions">
+          <ul class="transactions">
             ${block.transactions.map((transaction, i) => {
               const amount = formatEth(parseInt(transaction.value, 16))
 
               return `
-                <div class="transaction">
+                <li class="transaction">
                   <div class="sender" data-behavior="identicon" title="${transaction.from}"></div>
                   <div class="sent">sent</div>
                   <div class="amount">${amount}</div>
                   <div class="to">to</div>
                   <div class="receiver" data-behavior="identicon" title="${transaction.to}"></div>
-                </div>
+                </li>
               `
             }).join("")}
-          </div>
+          </ul>
         </div>
       </div>
     </div>
